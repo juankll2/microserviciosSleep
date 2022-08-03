1ca39b59fa333e8c6c8d38a647cc408338c1c9e7
Juan Moyano <juan.cranex@hotmail.com>
2022-08-03 00:03:44 -0500

    one file

---
+++ file.cmd
@@ -0,0 +1,5 @@
+## mvn clean install &
+start java -jar .\discovery-service\target\discovery-service-0.0.1-SNAPSHOT.jar
+start java -jar .\api-gateway\target\api-gateway-0.0.1-SNAPSHOT.jar
+start java -jar .\temperatura-service\target\temperatura-service-0.0.1-SNAPSHOT.jar
+start java -jar .\sonido-service\target\sonido-service-0.0.1-SNAPSHOT.jar
