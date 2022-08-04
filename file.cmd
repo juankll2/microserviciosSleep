f42e2a51c7b7bfe986846b001b49f3556e7633f8
Juan Moyano <juan.cranex@hotmail.com>
2022-08-03 19:41:05 -0500

    update

--- file.cmd
+++ file.cmd
@@ -3,3 +3,5 @@ start java -jar .\discovery-service\target\discovery-service-0.0.1-SNAPSHOT.jar
 start java -jar .\api-gateway\target\api-gateway-0.0.1-SNAPSHOT.jar
 start java -jar .\temperatura-service\target\temperatura-service-0.0.1-SNAPSHOT.jar
 start java -jar .\sonido-service\target\sonido-service-0.0.1-SNAPSHOT.jar
+
+az spring-cloud app deploy -n api-gateway -s spring-cloud-fun -g spring-cloud-fun-group --jar-path ./api-gateway/target/api-gateway-0.0.1-SNAPSHOT.jar
